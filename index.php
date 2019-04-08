a4ae0acaae15fc81fb9eb8cd5f8e5a72d895ab63
Gaganugraha92 <gaganugraha33@gmail.com>
2019-04-08 16:19:33 +0700

    asd
    asdasd

--- index.php
+++ index.php
@@ -18,15 +18,16 @@
  <body>
  <h1>Register here!</h1>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" >
+ <form method="post" action="index.php" enctype="multipart/form-data" >
        Name  <input type="text" name="name" id="name"/></br></br>
-       Address<input type="text" name="address" id="address"/></br></br>
+       Email <input type="text" name="email" id="email"/></br></br>
+       Job <input type="text" name="job" id="job"/></br></br>
        <input type="submit" name="submit" value="Submit" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "submissionazure1db.database.windows.net";
-    $user = "gaganugraha";
+    $host = "https://submissionazure1.azurewebsites.net";
+    $user = "Gaganugraha";
     $pass = "Rahasia123";
     $db = "Registration";
     try {
@@ -38,13 +39,17 @@
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
-            $address = $_POST['address'];
-            
-            $sql_insert = "INSERT INTO Users (Name, Address) 
-                        VALUES (?,?)";
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
+            // Insert data
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                        VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $address);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
@@ -52,17 +57,21 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Users";
+            $sql_select = "SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
-                echo "<th>Address</th></tr>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['Name']."</td>";
-                    echo "<td>".$registrant['Address']."</td></tr>";
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
                 }
                 echo "</table>";
             } else {
